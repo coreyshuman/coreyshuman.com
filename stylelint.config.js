d81d72e54b82dfbc613f571ac36af832e3ff6645
Corey Shuman <ctshuman89@gmail.com>
2023-11-20 03:00:54 -0800

    add Vale to project for prose style linting

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,6 +3,6 @@ module.exports = {
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {
-    "at-rule-no-unknown": null,
+    'at-rule-no-unknown': null
   }
 };
